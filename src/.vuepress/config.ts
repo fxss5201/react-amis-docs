7cf19993d16f186a0aa383881526f73ef6353e5d
fxss <723107234@qq.com>
2023-06-11 19:28:04 +0800

    feat: 添加百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,11 +4,27 @@ import { cut } from "nodejs-jieba";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/react-amis-docs/",
 
   lang: "zh-CN",
   title: "泛积木-低代码",
   description: "泛积木-低代码：可以广泛使用的积木框架",
+
+  head: [
+    [
+      'script', {},
+      `<script>
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?31a3c177589348e7ac3e6bbf638624d7";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      </script>
+      `
+    ]
+  ],
 
   theme,
 
